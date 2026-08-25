50a76685232f4e07c1fe244ed65302f3433aa6bb
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2026-08-26 00:20:48 +0430

    updated auth.ts to make sure it creates session for user

--- auth.ts
+++ auth.ts
@@ -4,6 +4,8 @@ import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
+
   providers: [
     Credentials({
       credentials: {
@@ -52,4 +54,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+  },
 });
